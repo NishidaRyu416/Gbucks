de018257c042d6583c73afdc7b1a7a4c75b72714
NishidaRyu416 <nishidaryu416@gmail.com>
2017-09-17 10:11:06 +0900

    repair

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :drink
-  validates :stars,presence: true
+  validate :stars_checker
+  def stars_checker
+    check=stars.to_i
+    if check<1
+      errors.add(:stars,"エラー(星は1~5の五段階評価です)")
+    end
+  end
 end
